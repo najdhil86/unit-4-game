6daa2f460778b4c0db36c82a5a0ea4c7070be6d2
Naj Dhillon <najdhil86@gmail.com>
2019-07-15 18:20:57 -0700

    Work on Crystal Game

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+//Display Computer's random choice
+
+var randomTotalNumber = Math.floor(Math.random()*100);
+
+console.log(randomTotalNumber);
+
+
+$('#randomTotalNumber').text(randomTotalNumber);
+
+bugs 
+
+blocker
+
+Progress 
+
+
